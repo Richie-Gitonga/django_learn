2d68f5319ef5614f9b64a81f1f379666d300113c
Richie-Gitonga <gitongar00@gmail.com>
2025-01-22 21:46:26 +0300

    added publish managers for post model

--- bongohealth/post/models.py
+++ bongohealth/post/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+class PublishManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -24,6 +29,8 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+    published = PublishManager()
 
     class Meta:
         ordering = ['-publish']
