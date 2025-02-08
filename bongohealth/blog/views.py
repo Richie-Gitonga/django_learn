0975d49ebda57b2a508418f4bc170d340724d0f2
Richie-Gitonga <gitongar00@gmail.com>
2025-02-08 23:17:19 +0300

    added similar posts to post detail page

--- bongohealth/blog/views.py
+++ bongohealth/blog/views.py
@@ -5,6 +5,7 @@ from .models import Post
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .forms import EmailPostForm, CommentForm
 from taggit.models import Tag
+from django.db.models import Count
 
 # Create your views here.
 def post_list(request, tag_slug=None):
@@ -35,22 +36,6 @@ def post_list(request, tag_slug=None):
             'posts': posts,
             'tag': tag,
         }
-    )
-
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(
-        Post,
-        status=Post.Status.PUBLISHED,
-        slug=post,
-        publish__year=year,
-        publish__month=month,
-        publish__day=day
-    )
-
-    return render(
-        request,
-        'blog/post/detail.html',
-        {'post': post}
     )
 
 def post_share(request, post_id):
@@ -137,12 +122,21 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:6]
     return render(
         request,
         'blog/post/detail.html',
         {
             'post': post,
             'comments': comments,
-            'form': form
+            'form': form,
+            'similar_posts': similar_posts
         }
     )
